34c3ba03caa06d3a598d8dc8b8b692b8b498d7e3
viniciuspadovam <padovam3373@gmail.com>
2022-11-28 22:18:35 -0300

    refatorado model para uso do lombok

--- backend/src/main/java/com/vinicius/lojinha/model/Product.java
+++ backend/src/main/java/com/vinicius/lojinha/model/Product.java
@@ -2,13 +2,17 @@ package com.vinicius.lojinha.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,35 +21,4 @@ public class Product {
 	private String name;
 	private BigDecimal price;
 	private LocalDate entryStock;
-	
-	public Product() {
-		
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public BigDecimal getPrice() {
-		return price;
-	}
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-	public LocalDate getEntryStock() {
-		return entryStock;
-	}
-	public void setEntryStock(LocalDate entryStock) {
-		this.entryStock = entryStock;
-	}
-	
-	
 }
